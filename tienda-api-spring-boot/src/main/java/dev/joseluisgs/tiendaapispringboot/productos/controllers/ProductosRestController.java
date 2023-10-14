0274b52bcce4a298e58603167afc9596e52fcd4b
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-10-14 17:55:09 +0200

    Refactor Controlador Producto

--- tienda-api-spring-boot/src/main/java/dev/joseluisgs/tiendaapispringboot/productos/controllers/ProductosRestController.java
+++ tienda-api-spring-boot/src/main/java/dev/joseluisgs/tiendaapispringboot/productos/controllers/ProductosRestController.java
@@ -1,7 +1,11 @@
 package dev.joseluisgs.tiendaapispringboot.productos.controllers;
 
+import dev.joseluisgs.tiendaapispringboot.exceptions.ProductoBadRequest;
+import dev.joseluisgs.tiendaapispringboot.exceptions.ProductoNotFound;
 import dev.joseluisgs.tiendaapispringboot.productos.models.Producto;
-import dev.joseluisgs.tiendaapispringboot.productos.repositories.ProductosRepository;
+import dev.joseluisgs.tiendaapispringboot.services.ProductosService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,42 +25,42 @@ import java.util.List;
 
 @RequestMapping("${api.version}/productos") // Es la ruta del controlador
 public class ProductosRestController {
-
     // Repositorio de productos
-    private final ProductosRepository productosRepository;
+    private final ProductosService productosService;
+    private final Logger logger = LoggerFactory.getLogger(ProductosRestController.class);
 
     @Autowired
-    public ProductosRestController(ProductosRepository productosRepository) {
-        this.productosRepository = productosRepository;
+    public ProductosRestController(ProductosService productosService) {
+        this.productosService = productosService;
     }
 
     /**
      * Obtiene todos los productos
      *
-     * @param marca, si se quiere filtrar por marca se pasa como parámetro de consulta en la URL ?marca=valor y se filtra, no es obligatorio
-     *               Si no se pasa, se devuelven todos los productos
+     * @param marca     Marca del producto
+     * @param categoria Categoria del producto
      * @return Lista de productos
      */
     @GetMapping()
-    public ResponseEntity<List<Producto>> getAllProducts(@RequestParam(required = false) String marca) {
-        if (marca != null) {
-            return ResponseEntity.ok(productosRepository.findAllByMarca(marca));
-        } else {
-            return ResponseEntity.ok(productosRepository.findAll());
-        }
+    public ResponseEntity<List<Producto>> getAllProducts(
+            @RequestParam(required = false) String marca,
+            @RequestParam(required = false) String categoria
+    ) {
+        logger.info("Buscando todos los productos con marca: " + marca + " y categoría: " + categoria);
+        return ResponseEntity.ok(productosService.findAll(marca, categoria));
     }
 
     /**
      * Obtiene un producto por su id
      *
      * @param id del producto, se pasa como parámetro de la URL /{id}
-     * @return Producto si existe, si no, Not Found
+     * @return Producto si existe
+     * @throws ProductoNotFound si no existe el producto (404)
      */
     @GetMapping("/{id}")
     public ResponseEntity<Producto> getProductById(@PathVariable Long id) {
-        return productosRepository.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        logger.info("Buscando producto por id: " + id);
+        return ResponseEntity.ok(productosService.findById(id));
     }
 
     /**
@@ -64,11 +68,12 @@ public class ProductosRestController {
      *
      * @param producto a crear
      * @return Producto creado
+     * @throws ProductoBadRequest si el producto no es correcto (400)
      */
     @PostMapping()
     public ResponseEntity<Producto> createProduct(@RequestBody Producto producto) {
-        var saved = productosRepository.save(producto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+        logger.info("Creando producto: " + producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productosService.save(producto));
     }
 
     /**
@@ -77,16 +82,13 @@ public class ProductosRestController {
      * @param id       del producto a actualizar, se pasa como parámetro de la URL /{id}
      * @param producto a actualizar
      * @return Producto actualizado
+     * @throws ProductoNotFound   si no existe el producto (404)
+     * @throws ProductoBadRequest si el producto no es correcto (400)
      */
     @PutMapping("/{id}")
     public ResponseEntity<Producto> updateProduct(@PathVariable Long id, @RequestBody Producto producto) {
-        // Lo buscamos, si existe lo actualizamos, si no devolvemos Not Found
-        return productosRepository.findById(id)
-                .map(p -> {
-                    var updated = productosRepository.save(producto);
-                    return ResponseEntity.status(HttpStatus.CREATED).body(updated);
-                })
-                .orElse(ResponseEntity.notFound().build());
+        logger.info("Actualizando producto por id: " + id + " con producto: " + producto);
+        return ResponseEntity.ok(productosService.update(id, producto));
     }
 
     /**
@@ -95,32 +97,26 @@ public class ProductosRestController {
      * @param id       del producto a actualizar, se pasa como parámetro de la URL /{id}
      * @param producto a actualizar
      * @return Producto actualizado
+     * @throws ProductoNotFound   si no existe el producto (404)
+     * @throws ProductoBadRequest si el producto no es correcto (400)
      */
     @PatchMapping("/{id}")
     public ResponseEntity<Producto> updatePartialProduct(@PathVariable Long id, @RequestBody Producto producto) {
-        // Lo buscamos, si existe lo actualizamos, si no devolvemos Not Found
-        return productosRepository.findById(id)
-                .map(p -> {
-                    var updated = productosRepository.save(producto);
-                    return ResponseEntity.status(HttpStatus.CREATED).body(updated);
-                })
-                .orElse(ResponseEntity.notFound().build());
+        logger.info("Actualizando parcialmente producto por id: " + id + " con producto: " + producto);
+        return ResponseEntity.ok(productosService.update(id, producto));
     }
 
     /**
      * Borrar un producto
      *
      * @param id del producto, se pasa como parámetro de la URL /{id}
-     * @return No Content si se ha borrado, si no, Not Found
+     * @return No Content si se ha borrado
+     * @throws ProductoNotFound si no existe el producto (404)
      */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        // Lo buscamos, si existe lo borramos, si no devolvemos Not Found
-        return productosRepository.findById(id)
-                .map(p -> {
-                    productosRepository.deleteById(id);
-                    return ResponseEntity.status(HttpStatus.NO_CONTENT).<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        logger.info("Borrando producto por id: " + id);
+        productosService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
