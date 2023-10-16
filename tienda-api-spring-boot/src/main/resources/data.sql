0d28b18cb1bb19c9551669163999507bc413ca5b
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-10-16 12:47:49 +0200

    Ajuste de datos sql

--- tienda-api-spring-boot/src/main/resources/data.sql
+++ tienda-api-spring-boot/src/main/resources/data.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS PRODUCTOS (
 
 -- Datos de ejemplo CATEGORIAS
 INSERT INTO CATEGORIAS (nombre)
-VALUES ('DEPORTE'),
+VALUES ('DEPORTES'),
        ('COMIDA'),
        ('BEBIDA'),
        ('COMPLEMENTOS'),
@@ -33,9 +33,9 @@ VALUES ('DEPORTE'),
 
 
 -- Datos de ejemplo PRODUCTOS
-INSERT INTO PRODUCTOS (marca, modelo, descripcion, precio, imagen, categoria, stock, uuid)
-VALUES ('Nike', 'Modelo1', 'Descripción1', 10.99, 'https://via.placeholder.com/150', 'DEPORTE', 5, UUID()),
-       ('Adidas', 'Modelo2', 'Descripción2', 19.99, 'https://via.placeholder.com/150', 'OTROS', 10, UUID()),
-       ('Nike', 'Modelo3', 'Descripción3', 15.99, 'https://via.placeholder.com/150', 'DEPORTE', 2, UUID()),
-       ('Nike', 'Modelo4', 'Descripción4', 25.99, 'https://via.placeholder.com/150', 'OTROS', 8, UUID()),
-       ('Adidas', 'Modelo5', 'Descripción5', 12.99, 'https://via.placeholder.com/150', 'OTROS', 3, UUID());
+INSERT INTO PRODUCTOS (marca, modelo, descripcion, precio, imagen, categoria_id, stock, uuid)
+VALUES ('Nike', 'Modelo1', 'Descripción1', 10.99, 'https://via.placeholder.com/150', 1, 5, UUID()),
+       ('Adidas', 'Modelo2', 'Descripción2', 19.99, 'https://via.placeholder.com/150', 5, 10, UUID()),
+       ('Nike', 'Modelo3', 'Descripción3', 15.99, 'https://via.placeholder.com/150', 1, 2, UUID()),
+       ('Nike', 'Modelo4', 'Descripción4', 25.99, 'https://via.placeholder.com/150', 5, 8, UUID()),
+       ('Adidas', 'Modelo5', 'Descripción5', 12.99, 'https://via.placeholder.com/150', 5, 3, UUID());
