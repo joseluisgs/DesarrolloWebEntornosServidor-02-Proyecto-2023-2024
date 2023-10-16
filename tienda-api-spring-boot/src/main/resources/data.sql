4a839dceb1f80a1b75293ab8c15609fde27bf8b6
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-10-16 12:14:48 +0200

    Categorias datos de ejemplo y mejoras en properties

--- tienda-api-spring-boot/src/main/resources/data.sql
+++ tienda-api-spring-boot/src/main/resources/data.sql
@@ -1,4 +1,6 @@
 /*
+
+Ejemplo de si queremos hacer las tablas a mano y no con JPA
 -- Borra la tabla si existe
 DROP TABLE IF EXISTS PRODUCTOS;
 
@@ -21,7 +23,16 @@ CREATE TABLE IF NOT EXISTS PRODUCTOS (
 
 */
 
--- Filas de ejemplo
+-- Datos de ejemplo CATEGORIAS
+INSERT INTO CATEGORIAS (nombre)
+VALUES ('DEPORTE'),
+       ('COMIDA'),
+       ('BEBIDA'),
+       ('COMPLEMENTOS'),
+       ('OTROS');
+
+
+-- Datos de ejemplo PRODUCTOS
 INSERT INTO PRODUCTOS (marca, modelo, descripcion, precio, imagen, categoria, stock, uuid)
 VALUES ('Nike', 'Modelo1', 'Descripción1', 10.99, 'https://via.placeholder.com/150', 'DEPORTE', 5, UUID()),
        ('Adidas', 'Modelo2', 'Descripción2', 19.99, 'https://via.placeholder.com/150', 'OTROS', 10, UUID()),
