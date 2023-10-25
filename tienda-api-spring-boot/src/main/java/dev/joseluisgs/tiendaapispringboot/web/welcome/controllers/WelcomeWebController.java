ef129c803d5b9229533d8f70957a500df7405f7d
José Luis González Sánchez <joseluisgonsan@gmail.com>
2023-10-25 11:16:58 +0200

    Localizacion

--- tienda-api-spring-boot/src/main/java/dev/joseluisgs/tiendaapispringboot/web/welcome/controllers/WelcomeWebController.java
+++ tienda-api-spring-boot/src/main/java/dev/joseluisgs/tiendaapispringboot/web/welcome/controllers/WelcomeWebController.java
@@ -1,4 +1,4 @@
-package dev.joseluisgs.tiendaapispringboot.web.welcome;
+package dev.joseluisgs.tiendaapispringboot.web.welcome.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
